d9255bae7f09958654f1c84366be4e42442f1bd5
Dedi Fardiyanto <dedif15@gmail.com>
2024-12-22 23:48:54 +0700

    wisata app first init
    Signed-off-by: ITCS - Dedi Fardiyanto <v.dedi.fardiyanto@adira.co.id>

---
+++ app.vue
@@ -0,0 +1,15 @@
+<!--
+  - Author: Dedi Fardiyanto Copyright (c) 2024.
+  -
+  - Created At: 12/22/24, 11:48 PM
+  - Filename: app.vue
+  - Last Modified: 12/22/24, 11:39 PM
+  -->
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+<!--    <h1>test aja ada</h1>-->
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+/*
+ * Author: Dedi Fardiyanto Copyright (c) 2024.
+ *
+ * Created At: 12/22/24, 11:48 PM
+ * Filename: nuxt.config.ts
+ * Last Modified: 11/20/24, 11:44 PM
+ */
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
